230d37c54f7400cf1dfd689c845ab6297c14febd
Khachornchit Songsaen <khachornchit.songsaen@gmail.com>
2019-09-14 16:28:02 +0700

    Java Generic

---
+++ src/Core09/Generic/Node.java
@@ -0,0 +1,14 @@
+package Core09.Generic;
+
+public class Node<T> {
+	T data;
+	Node<T> node;
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+}
